955970dc50a36e24586a9703381b746632ad9cd2
Dean Howe <hideanhowe@gmail.com>
2025-12-05 21:47:01 +0000

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+// function difference(arr1, arr2) {
+// console.log("Start with: ",arr1, arr2)
+// create a set
+// const set = new Set();
+// spread both arrays;
+// const both = [...arr1, ...arr2];
+// console.log("Spread both: ",both)
+// set.add(both);
+// console.log("Set for unique elements: ", set)
+// const resultArr = Array.from(set)
+// console.log(resultArr)
+//  return arr1;
+//}
