965893a9f1285657a34b3d92f0073efd65ec2b25
Dean Howe <hideanhowe@gmail.com>
2025-12-05 21:51:15 +0000

    logic

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Symmetric Difference
+// Given two arrays, return a new array containing the symmetric difference of them.
+
+// The symmetric difference between two sets is the set of values that appear in either set, but not both.
+// Return the values in the order they first appear in the input arrays.
+
 // function difference(arr1, arr2) {
 // console.log("Start with: ",arr1, arr2)
 // create a set
@@ -11,3 +17,14 @@
 // console.log(resultArr)
 //  return arr1;
 //}
+
+// think I am barking up the wrong tree
+// take the first element in arr1
+// check if it does not exist in arr2
+// add it to new array
+//
+
+// think I am barking up the wrong tree
+// put both arrays into one array
+// add to a set
+// return array of set
